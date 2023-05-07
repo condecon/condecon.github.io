6ab2dd4f71f7a2c557adcc390ae76ec226907a24
Jonas Engicht <jonas.engicht@uni-jena.de>
2023-05-07 11:00:45 +0200

    update page

--- about.markdown
+++
@@ -1,7 +0,0 @@
----
-layout: single
-title: About
-permalink: /about
----
-
-As a Bachelor of Science psychology student and a young researcher, I am fascinated by computer science and its applications. I have acquired skills in various programming languages, such as R, Python, Dart, C#, JAVA and some C/C++. My goal is to deepen my knowledge of data science and machine learning, and to use them to tackle real-world problems. I am always curious about new technologies and challenges, and I welcome new opportunities and collaborations.
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Introducing psytoolkittools: A new R package for PsyToolkit data analysis",
+        "excerpt":"PsyToolkit is a free platform for online psychological research and information. It allows you to create and run cognitive-psychological experiments and surveys, including personality tests, in your browser. However, if you want to analyze your PsyToolkit data in R, you might encounter some challenges. For example, how do you import...","categories": [],
+        "tags": [],
+        "url": "/2023/05/06/psytoolkittools-introduction.html",
+        "teaser": null
+      }]
